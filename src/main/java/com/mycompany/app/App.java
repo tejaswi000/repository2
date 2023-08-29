2a8665845095423747c347270953b1a15543276b
tejaswi000 <nandeti.tejaswi@gmail.com>
2023-08-29 16:59:39 +0800

    first commit

---
+++ src/main/java/com/mycompany/app/App.java
@@ -0,0 +1,13 @@
+package com.mycompany.app;
+
+/**
+ * hello tejaswi
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hyy Teja...." );
+    }
+}
